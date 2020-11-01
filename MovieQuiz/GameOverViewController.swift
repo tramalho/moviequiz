52e866d18410b7a447b260b4167aa23204237376
tramalho <thiagofacul@gmail.com>
2020-10-31 23:34:41 -0300

    122. Utilizando AVPlayer

--- MovieQuiz/GameOverViewController.swift
+++ MovieQuiz/GameOverViewController.swift
@@ -11,12 +11,12 @@ import UIKit
 class GameOverViewController: UIViewController {
 
     @IBOutlet weak var score: UILabel!
+    var totalScore:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        score.text = "\(totalScore)"
     }
-    
 
     @IBAction func playAgain(_ sender: Any) {
         dismiss(animated: true, completion: nil)
--- MovieQuiz/ViewController.swift
+++ MovieQuiz/ViewController.swift
@@ -16,9 +16,13 @@ class ViewController: UIViewController {
     @IBOutlet var btOptions: [UIButton]!
     @IBOutlet weak var ivQuiz: UIImageView!
     @IBOutlet weak var timer: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var background: UIImageView!
     
     private var quizManager:QuizManager!
     private var avAudioPlayer: AVAudioPlayer!
+    private var playerItem: AVPlayerItem!
+    private var backgroundMusicPlayer: AVPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,13 +40,13 @@ class ViewController: UIViewController {
         for (index, element) in random.options.enumerated() {
             btOptions[index].setTitle(element.name, for: .normal)
         }
-        
+        playBackgroundMusic()
         play()
     }
     
     private func startTimer() {
-        timer.frame = view.frame
-        UIView.animate(withDuration: 60.0, delay: 0.0, options: .curveLinear, animations: {
+        timer.frame = background.frame
+        UIView.animate(withDuration: 35.0, delay: 0.0, options: .curveLinear, animations: {
             self.timer.frame.size.width = 0
             self.timer.frame.origin.x = self.view.center.x
         }) { (success) in
@@ -53,6 +57,7 @@ class ViewController: UIViewController {
     private func gameOver() {
         performSegue(withIdentifier: "gameOverSegue", sender: nil)
         avAudioPlayer.stop()
+        backgroundMusicPlayer.pause()
     }
     
     private func play() {
@@ -73,6 +78,26 @@ class ViewController: UIViewController {
         }
     }
     
+    private func playBackgroundMusic() {
+        soundBar.isHidden = false
+        playButton.setImage(UIImage(named: "pause"), for: .normal)
+        guard let url = Bundle.main.url(forResource: "countdown-30", withExtension: "mp3") else { return }
+        playerItem = AVPlayerItem(url: url)
+        backgroundMusicPlayer = AVPlayer(playerItem: playerItem)
+        backgroundMusicPlayer.volume = 0.1
+        backgroundMusicPlayer.play()
+        backgroundMusicPlayer.addPeriodicTimeObserver(forInterval: CMTimeMakeWithSeconds(1, preferredTimescale: 1), queue: nil) { (time) in
+            let percent = time.seconds / self.playerItem.duration.seconds
+            self.slider.setValue(Float(percent), animated: true)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? GameOverViewController {
+            vc.totalScore = quizManager.score
+        }
+    }
+    
     @IBAction func playQuiz(_ sender: UIButton) {
         play()
     }
@@ -86,13 +111,30 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showSoundBar(_ sender: UIButton) {
-        sender.isHidden = !sender.isHidden
+        soundBar.isHidden = !soundBar.isHidden
     }
     
-    @IBAction func changeMusicStatus(_ sender: Any) {
+    @IBAction func changeMusicStatus(_ sender: UIButton) {
+        
+        var imageName = ""
+        
+        if backgroundMusicPlayer.timeControlStatus == .paused {
+            backgroundMusicPlayer.play()
+            imageName = "pause"
+        } else {
+            backgroundMusicPlayer.pause()
+            imageName = "play"
+        }
+        
+        playButton.setImage(UIImage(named: imageName), for: .normal)
     }
     
-    @IBAction func changeMusicTime(_ sender: Any) {
+    @IBAction func changeMusicTime(_ sender: UISlider) {
+        backgroundMusicPlayer.seek(to: CMTime(seconds: Double(sender.value) * playerItem.duration.seconds, preferredTimescale: 1))
+        
+        if backgroundMusicPlayer.timeControlStatus == .paused {
+            backgroundMusicPlayer.play()
+        }
     }
 }
 
